0132d4ec4326d00b4914a7d384bca6a631659884
latest <charles.jacquin@autistici.org>
2016-08-15 22:21:26 +0200

    feat(exercice 1 start):

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,37 @@ import 'basscss/css/basscss.css';
 import React from 'react';
 import config from './_config';
 
+const styles = {
+  borderRadius: '50%',
+};
+
 export default () => {
   return (
     <section>
       <h1>{config.APP_TITLE}</h1>
       <div>Webpack is doing its thing with React and ES2015</div>
+      <div className="flex mb2 border flex-center">
+        <img
+          style={styles}
+          src="img/placeholder.svg"
+          width="96"
+          height="96"
+          className="flex-none m2"
+          role="presentation"
+        />
+        <div className="flex-auto p2 border">
+          <h3>John snow</h3>
+          <p>Lord commander</p>
+          <p>
+            <button
+              type="button"
+              className="btn btn-primary white"
+            >
+            Do something
+            </button>
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
