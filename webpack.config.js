aa46812739185f4988e5dbd79a74f8eb8e572a7d
latest <charles.jacquin@autistici.org>
2016-08-19 00:18:46 +0200

    style(style): webpack.config => eslint

--- webpack.config.js
+++ webpack.config.js
@@ -11,53 +11,53 @@ module.exports = webpackMerge.smart(require(`./webpack/${ENV}`), {
       browser: path.join(__dirname, 'src/platform/browser'),
     },
     extensions: ['', '.jsx', '.js', '.json'],
-    modulesDirectories: ['node_modules']
+    modulesDirectories: ['node_modules'],
   },
   module: {
     preLoaders: [
-     {
-       test: /\.jsx|.js$/,
-       loaders: ['eslint-loader', 'source-map-loader'],
-       exclude: /node_modules/,
-     },
-   ],
+      {
+        test: /\.jsx|.js$/,
+        loaders: ['eslint-loader', 'source-map-loader'],
+        exclude: /node_modules/,
+      },
+    ],
     loaders: [
       {
         test: /\.jsx|.js$/,
         exclude: /node_modules/,
-        loader: "babel",
+        loader: 'babel',
         query: {
-          presets: [ 'es2015-native-modules', 'stage-0', 'react' ],
+          presets: ['es2015-native-modules', 'stage-0', 'react'],
           plugins: ['transform-decorators-legacy'],
-        }
+        },
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
+        loaders: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(gif|png|jpe?g)$/i,
-        loader: 'file?name=dist/img/[name].[ext]'
+        loader: 'file?name=dist/img/[name].[ext]',
       },
       {
         test: /\.woff2?$/,
-        loader: 'url?name=dist/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff'
-       },
+        loader: 'url?name=dist/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff',
+      },
       {
         test: /\.(ttf|eot|svg)$/,
-        loader: 'file?name=dist/fonts/[name].[ext]'
-      }
-    ]
+        loader: 'file?name=dist/fonts/[name].[ext]',
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': `"${ENV}"`
-      }
+        NODE_ENV: `"${ENV}"`,
+      },
     }),
-  ]
-})
+  ],
+});
