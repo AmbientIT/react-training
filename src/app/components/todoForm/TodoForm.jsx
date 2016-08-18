bb45b2586ff71f8faf2977d1822a3ed28899648c
latest <charles.jacquin@autistici.org>
2016-08-18 15:17:22 +0200

    feat(form): add validation

--- src/app/components/todoForm/TodoForm.jsx
+++ src/app/components/todoForm/TodoForm.jsx
@@ -20,12 +20,28 @@ const domOnlyProps = ({
   autofilled,
   ...domProps }) => domProps;
 
+const validate = values => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Required';
+  } else if (values.title.length > 15) {
+    errors.title = 'Must be 15 characters or less';
+  }
+  if (!values.description) {
+    errors.description = 'Required';
+  } else if (values.description.length > 40) {
+    errors.description = 'Must be 15 characters or less';
+  }
+  return errors;
+};
+
 @reduxForm(
   {
     form: 'todo',
     fields: ['title', 'description'],
+    validate,
   },
-  state => ({ initialValues: state.todos.selectedTodo.toObject() })
+  state => ({ initialValues: state.todos.selectedTodo.toObject() }),
 )
 export default class TodoForm extends Component {
   static propTypes = {
@@ -52,6 +68,7 @@ export default class TodoForm extends Component {
           placeholder="Enter title..."
           reduxdata={domOnlyProps(title)}
         />
+        {title.touched && title.error && <div>{title.error}</div>}
 
         <Label key="description">Description</Label>
         <Input
@@ -59,6 +76,8 @@ export default class TodoForm extends Component {
           placeholder="Enter description..."
           reduxdata={domOnlyProps(description)}
         />
+        {description.touched && description.error && <div>{description.error}</div>}
+
         <Button status="success" type="submit" disabled={submitting}>Save</Button>
       </Form>
     );
