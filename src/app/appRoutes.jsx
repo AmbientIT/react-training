d2412d263f22f642f7f837f94157da9ffae1c67d
latest <charles.jacquin@autistici.org>
2016-08-21 12:46:34 +0200

    style(appRoutes): remove spaces

--- src/app/appRoutes.jsx
+++ src/app/appRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router,	Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './App';
 import Home from './containers/home/Home';
 import Todo from './containers/todo/Todo';
@@ -7,7 +7,7 @@ import Todo from './containers/todo/Todo';
 export const appRoutes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute	component={Home}	/>
+      <IndexRoute component={Home} />
       <Route path="/todo" component={Todo} />
     </Route>
   </Router>
