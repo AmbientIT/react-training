651fd620a770c1faffb37858e179e647b976e972
latest <charles.jacquin@autistici.org>
2016-08-22 20:02:26 +0200

    style(appRoutes): move file

--- src/platform/browser/App.jsx
+++ src/platform/browser/App.jsx
@@ -3,7 +3,7 @@ import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from '../common/store';
-import createRoutes from '../common/appRoutes';
+import createRoutes from './appRoutes';
 
 if (process.env.NODE_ENV === 'development') {
   require('./styles/bass.css');
